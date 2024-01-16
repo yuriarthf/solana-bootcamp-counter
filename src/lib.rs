1dc8cae45e545588aafbc150af1c0e2fffa8ba13
Yuri Fernandes <yuriarthf@protonmail.com>
2024-01-16 22:23:07 +0700

    update tests

--- src/lib.rs
+++ src/lib.rs
@@ -84,26 +84,30 @@ mod test {
 
         let accounts = vec![account];
 
-        let increment_instruction_data = vec![0];
-        let decrement_instruction_data = vec![1];
+        let mut increment_instruction_data = vec![0];
+        let mut decrement_instruction_data = vec![1];
         let mut update_instruction_data = vec![2];
         let reset_instruction_data = vec![3];
 
+        let increment_value = 10u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
         process_instruction(
             &program_id, 
             &accounts, 
             &increment_instruction_data
         ).unwrap();
 
-        assert_eq!(CounterAccount::try_from_slice(&accounts[0].data.borrow()).unwrap().counter, 1);
+        assert_eq!(CounterAccount::try_from_slice(&accounts[0].data.borrow()).unwrap().counter, increment_value);
 
+        let decrement_value = 5u32;
+        decrement_instruction_data.extend_from_slice(&decrement_value.to_le_bytes());
         process_instruction(
             &program_id, 
             &accounts, 
             &decrement_instruction_data
         ).unwrap();
 
-        assert_eq!(CounterAccount::try_from_slice(&accounts[0].data.borrow()).unwrap().counter, 0);
+        assert_eq!(CounterAccount::try_from_slice(&accounts[0].data.borrow()).unwrap().counter, increment_value - decrement_value);
 
         let update_value = 33u32;
         update_instruction_data.extend_from_slice(&update_value.to_le_bytes());
@@ -113,7 +117,7 @@ mod test {
             &update_instruction_data
         ).unwrap();
 
-        assert_eq!(CounterAccount::try_from_slice(&accounts[0].data.borrow()).unwrap().counter, 33);
+        assert_eq!(CounterAccount::try_from_slice(&accounts[0].data.borrow()).unwrap().counter, update_value);
 
         process_instruction(
             &program_id, 
